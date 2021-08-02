b8de22454fb7e461496f19aad931381e022cd211
Arda Gungor <ardagungor98@gmail.com>
2021-08-02 14:07:40 +0300

    filter by platform functionality added

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from "react";
 import classes from "./Table.module.css";
 import axios from "axios";
+import { Menu, Button, MenuButton, MenuList, MenuItem } from "@chakra-ui/react";
+import { ChevronDownIcon } from "@chakra-ui/icons";
 
 const Table = () => {
   const [data, setData] = useState([]);
+  const [table, setTable] = useState();
+  const [backup, setBackup] = useState([]);
   const [app, setApp] = useState(true);
   const [date, setDate] = useState(true);
   const [platform, setPlatform] = useState(true);
@@ -19,9 +23,9 @@ const Table = () => {
       method: "get",
     })
       .then((res) => {
-        console.log(res);
         setData(res.data.data);
-        console.log(data);
+        setBackup(res.data.data);
+        setTable(res.data.data);
       })
       .catch((err) => {
         console.log(err);
@@ -30,7 +34,26 @@ const Table = () => {
 
   useEffect(() => {
     loadData();
+    // setTable(
+    //   data.map((app) => {
+    //     return (
+    //       <tr>
+    //         <td style={app == true ? { display: "none" } : {}}>{app.app}</td>
+    //         <td>{app.date}</td>
+    //         <td>{app.platform}</td>
+    //         <td>{app.clicks}</td>
+    //         <td>{app.impressions}</td>
+    //         <td>{app.installs}</td>
+    //         <td>{app.dau}</td>
+    //         <td>{app.revenue.toString().slice(0, 8)}</td>
+    //       </tr>
+    //     );
+    //   })
+    // );
+    setTable(backup);
+    console.log(table);
   }, []);
+
   return (
     <div className={classes.container}>
       <div className={classes.controls}>
@@ -65,7 +88,9 @@ const Table = () => {
             className={
               clicks ? classes.colItem + " " + classes.active : classes.colItem
             }
-            onClick={() => setClicks(!clicks)}
+            onClick={() => {
+              setClicks(!clicks);
+            }}
           >
             Clicks
           </div>
@@ -113,7 +138,56 @@ const Table = () => {
           <tr>
             {app ? <td>App</td> : null}
             {date ? <td>Date</td> : null}
-            {platform ? <td>Platform</td> : null}
+            {platform ? (
+              <td>
+                Platform{" "}
+                <Menu>
+                  <MenuButton
+                    marginLeft="21px"
+                    as={Button}
+                    rightIcon={<ChevronDownIcon />}
+                  >
+                    Filter
+                  </MenuButton>
+                  <MenuList>
+                    <MenuItem
+                      onClick={() => {
+                        let filtered = data.filter(function (el) {
+                          return el.platform == "Android";
+                        });
+                        // let filtered = [];
+                        // for (let i = 0; i < data.length; i++) {
+                        //   if (data[i].platform == "Android") {
+                        //     filtered.push(data[i]);
+                        //   }
+                        // }
+
+                        setTable(filtered);
+                      }}
+                    >
+                      Android
+                    </MenuItem>
+                    <MenuItem
+                      onClick={() => {
+                        let filtered = data.filter(function (el) {
+                          return el.platform == "iOS";
+                        });
+                        // let filtered = [];
+                        // for (let i = 0; i < data.length; i++) {
+                        //   if (data[i].platform == "Android") {
+                        //     filtered.push(data[i]);
+                        //   }
+                        // }
+
+                        setTable(filtered);
+                      }}
+                    >
+                      iOS
+                    </MenuItem>
+                  </MenuList>
+                </Menu>
+              </td>
+            ) : null}
             {clicks ? <td>Clicks</td> : null}
             {impr ? <td>Impressions</td> : null}
             {installs ? <td>Installs</td> : null}
@@ -122,10 +196,16 @@ const Table = () => {
           </tr>
         </thead>
         <tbody>
-          {data.map((app) => {
+          {/* {data.map((app) => {
             return (
               <tr>
-                <td className={app ? null : classes.hidden}>{app.app}</td>
+                <td
+                  style={
+                    app === true ? { display: "block" } : { display: "none" }
+                  }
+                >
+                  {app.app}
+                </td>
                 <td>{app.date}</td>
                 <td>{app.platform}</td>
                 <td>{app.clicks}</td>
@@ -135,7 +215,54 @@ const Table = () => {
                 <td>{app.revenue.toString().slice(0, 8)}</td>
               </tr>
             );
-          })}
+          })} */}
+          {/* {table
+            ? table.map((app) => {
+                return (
+                  <tr key={app.app + app.dau + app.revenue + app.installs}>
+                    <td>{app.app}</td>
+                    <td>{app.date}</td>
+                    <td>{app.platform}</td>
+                    <td>{app.clicks}</td>
+                    <td>{app.impressions}</td>
+                    <td>{app.installs}</td>
+                    <td>{app.dau}</td>
+                    <td>
+                      {app.revenue ? app.revenue.toString().slice(0, 8) : null}
+                    </td>
+                  </tr>
+                );
+              })
+            : data.map((app) => {
+                return (
+                  <tr key={app.app + app.dau + app.revenue + app.installs}>
+                    <td>{app.app}</td>
+                    <td>{app.date}</td>
+                    <td>{app.platform}</td>
+                    <td>{app.clicks}</td>
+                    <td>{app.impressions}</td>
+                    <td>{app.installs}</td>
+                    <td>{app.dau}</td>
+                    <td>{app.revenue.toString().slice(0, 8)}</td>
+                  </tr>
+                );
+              })} */}
+          {table ? table.map((app) => {
+            return (
+              <tr key={app.app + app.dau + app.revenue + app.installs}>
+                <td>{app.app}</td>
+                <td>{app.date}</td>
+                <td>{app.platform}</td>
+                <td>{app.clicks}</td>
+                <td>{app.impressions}</td>
+                <td>{app.installs}</td>
+                <td>{app.dau}</td>
+                <td>
+                  {app.revenue ? app.revenue.toString().slice(0, 8) : null}
+                </td>
+              </tr>
+            );
+          }) : null}
         </tbody>
       </table>
     </div>
